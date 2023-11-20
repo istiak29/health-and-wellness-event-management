e0a2c7d0f29bdf525c8cdfa1a6156fedca927d60
Istiak Islam <tistiak@outlook.com>
2023-11-20 23:47:40 +0600

    initial setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABibtn8ivFGfOpcp5U2rmGivkFfD3JzDM",
+  authDomain: "health-and-wellness-even-d2d83.firebaseapp.com",
+  projectId: "health-and-wellness-even-d2d83",
+  storageBucket: "health-and-wellness-even-d2d83.appspot.com",
+  messagingSenderId: "628668042228",
+  appId: "1:628668042228:web:7acbd970446ed57f13c7a1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
